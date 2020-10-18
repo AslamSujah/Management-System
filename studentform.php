2e4ee497011d730007f89099e4c3537af37713ab
Sujah Ameer <ameersujah@yahoo.com>
2020-10-18 11:58:32 +0530

    Align the php files with header.php

--- studentform.php
+++ studentform.php
@@ -3,38 +3,11 @@
 <head>
 <meta charset="utf-8">
 <title>Student Forms</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"  href="http://localhost/management/bootstrap/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
-<script src="http://localhost/management/bootstrap/bootstrap.min.js"></script>
-<style>
-body{
-    font-family: Courgette;
-}
-.submit{
-  background-color: purple;
-  color:white;
-  text-size:24px;
-  padding: 6px;
-  border-radius: 5px;
-  border:1px solid white;
-  font-size: 24px;
-}
-.submit:hover{
-  background-color: white;
-  color: purple;
-  box-shadow: 0px 0px 20px white;
-}
-h1{
-	font-size: 14px;
-}
 
-td,th{
-  padding: 4px;
-  text-align: center;
-}
-</style>
-</head>
+<?php 
+	include "header.php";
+?>
+
 <?php
 $servername = "localhost";
 $username="root";
